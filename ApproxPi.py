4ee8b2fa185b991ef04d7c79b78f2341f94e7e10
Derek Babb <dbabb1@gmail.com>
2020-01-28 11:43:40 -0600

    Starter code for template for lab 3.

---
+++ ApproxPi.py
@@ -0,0 +1,20 @@
+#ApproxPi.py
+#Name:
+#Date:
+#Assignment:
+import math
+import time
+
+def main():
+    realPi = math.pi
+    
+    #ask user for decimal percision (up to 10)
+    #calculate pi using the approximation technique
+    #Loop until the level of percision is reached
+
+    end = time.time()
+
+    elapsedTime = end - start
+    print(elapsedTime)
+
+main()
