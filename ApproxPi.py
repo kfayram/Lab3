543f2aa108571bf970adc8f267a377f445918e68
Derek Babb <dbabb1@gmail.com>
2020-01-30 15:13:55 -0600

    Fixed start time in template.

--- ApproxPi.py
+++ ApproxPi.py
@@ -7,8 +7,10 @@ import time
 
 def main():
     realPi = math.pi
-    
+
     #ask user for decimal percision (up to 10)
+
+    start = time.time()
     #calculate pi using the approximation technique
     #Loop until the level of percision is reached
 
